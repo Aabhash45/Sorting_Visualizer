413d985863cd30d1fd4e8ffe77017ce8faccea26
Aabhash45 <62182821+Aabhash45@users.noreply.github.com>
2021-07-07 10:38:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require("express");
-const app = express();
-const server = require("http").createServer(app);
+import { combineReducers } from "redux";
+import { array } from "./array";
+import { algorithm } from "./algorithm";
+import { currentBubbleTwo } from "./bubbleSort";
+import { currentQuickTwo, pivot } from "./quickSort";
+import { currentSwappers } from "./swappers";
+import { currentHeapThree } from "./heapSort";
+import { currentSorted } from "./sorted";
+import { currentMergeX } from "./mergeSort";
+import { isRunning } from "./running";
 
-server.listen(1337);
-
-app.use(express.static(__dirname + "/../client"));
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/../client/index.html");
+export default combineReducers({
+  array,
+  algorithm,
+  currentBubbleTwo,
+  currentQuickTwo,
+  pivot,
+  currentSwappers,
+  currentHeapThree,
+  currentSorted,
+  currentMergeX,
+  isRunning,
 });
